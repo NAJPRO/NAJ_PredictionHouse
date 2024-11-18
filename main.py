b63389853650a3577a3fa582cd034df88285b192
Audin <audinjunior1@gmail.com>
2024-11-18 01:49:44 +0100

    commit 18/11/2024 01:49

--- main.py
+++ main.py
@@ -50,6 +50,6 @@ def predict():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
